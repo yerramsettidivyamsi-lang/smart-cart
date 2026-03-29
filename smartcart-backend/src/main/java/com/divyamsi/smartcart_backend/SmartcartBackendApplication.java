076f67998854fa568f7c5e17bbbf42664fd76bf2
Divyamsi <yerramsettidivyamsi@gmail.com>
2026-03-29 10:14:10 +0530

    Initial project setup

---
+++ smartcart-backend/src/main/java/com/divyamsi/smartcart_backend/SmartcartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.divyamsi.smartcart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcartBackendApplication.class, args);
+	}
+
+}
---
+++ smartcart-backend/src/test/java/com/divyamsi/smartcart_backend/SmartcartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyamsi.smartcart_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
